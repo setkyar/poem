88c5f4e50ec1dfa8502bb94fc9b8c6f5ec15ff97
Set Kyar Wa Lar <setkyar16@gmail.com>
2015-12-18 16:57:12 +0630

    remove ugly color & add some padding bc of autocomelete :sunglass:

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     var poems 		= ['<h3 class="title">အပြုံးတစ်ပွင့်သို့ တမ်းချင်း</h3><p>“ယဉ်သောချစ်သူ …<br>မျက်ဝန်းချင်းဆုံ<br>ကြည့်တစ်စုံကြောင့်<br>လှပနေခက် ၊ မျက်နှာထက်မှ<br>ခဏတာစွင့် ၊ ကြွေလွင့်ခဲ့သော<br>ပြုံးတစ်ပွင့်အား လွမ်းမိသူ … ”</p>', '<p>ကျွန်တော့် ဆုငွေ<br>ကြွေမျက်ရည်ကို<br>ရက်စက်တတ်လွန်း ၊ ဖုန်းတစ်ခွန်းကြောင့်<br>ခဏတာမဲ့ ၊ သူရခဲ့သော<br>အပြုံးတစ်ချက် ၊ မျက်ရည်စက်နှင့်<br>လဲ၍ရက လဲလိုသည် ။…။'];
     var rand_poem 	= poems[Math.floor(Math.random() * poems.length)];
 
-    var colors		= ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e'];
+    var colors		= ['#1abc9c', '#3498db', '#9b59b6', '#34495e'];
     var rand_color  = colors[Math.floor(Math.random() * colors.length)];
 
     $('#poem>.text').append(rand_poem);
